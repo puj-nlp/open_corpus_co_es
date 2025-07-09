b1c0b559cd1089088f3bfba3e0285ba2a5987d5b
gabriel moreno <morenoluis@javeriana.edu.co>
2025-07-09 10:13:33 -0400

    version(0.1.1a1): change the version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="open_corpus_co_es",
-    version="0.1.0",
+    version="0.1.1a1",
     description="Corpus en español de Colombia para PLN",
     author="Luis Gabriel Moreno Sandoval",
     author_email="morenoluis@javeriana.edu.co",
